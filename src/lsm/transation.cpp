68bc074fd322ebaa1fcfb7be67c7154c046fa883
PARADISE-PARADOX <whoamiparadise@gmail.com>
2025-11-13 21:59:59 +0800

    LAB5

--- src/lsm/transation.cpp
+++ src/lsm/transation.cpp
@@ -10,6 +10,7 @@
 #include <iostream>
 #include <memory>
 #include <mutex>
+#include <stdexcept>
 #include <thread>
 #include <vector>
 
@@ -38,6 +39,7 @@ TranContext::TranContext(uint64_t tranc_id, std::shared_ptr<LSMEngine> engine,
       tranManager_(std::move(tranManager)), isolation_level_(isolation_level) {
 
   // TODO: Lab 5.2 构造函数初始化
+  // 构造时记录下操作，方便后续WAL日志记录
   operations.emplace_back(Record::createRecord(tranc_id));
 }
 
@@ -124,7 +126,8 @@ std::optional<std::string> TranContext::get(const std::string &key) {
     query = engine_->get(key, this->tranc_id_);
   } else {
     if (read_map_.find(key) != read_map_.end()) {
-      query = read_map_[key];
+      query =
+          read_map_[key]; // read_map_保存首次读取的内容，这样就可以防止幻读。
     } else {
       query = engine_->get(key, this->tranc_id_);
       read_map_[key] = query;
@@ -144,11 +147,141 @@ std::optional<std::string> TranContext::get(const std::string &key) {
 
 bool TranContext::commit(bool test_fail) {
   // TODO: Lab 5.2 commit 实现
+  spdlog::info("TranContext--commit(): Starting commit for transaction ID={}",
+               tranc_id_);
+
+  auto isolation_level = get_isolation_level();
+
+  if (isolation_level == IsolationLevel::READ_UNCOMMITTED) {
+    //读未提交时需要随着单词的操作更新，不需要统一更新
+    operations.emplace_back(Record::commitRecord(this->tranc_id_));
+
+    // 记录后加入wal中
+    auto wal_success = tranManager_->write_to_wal(operations);
+
+    if (!wal_success) {
+      spdlog::error(
+          "TranContext--commit(): Failed to write WAL for transaction ID={}",
+          tranc_id_);
+
+      // wal日志写入失败，抛出错误
+      throw std::runtime_error("Writ into WAL failed");
+    }
+
+    engine_->memtable.put_("", "", tranc_id_); // 结束标志
+    isCommited = true;
+
+    // 更新最大的事务id
+    tranManager_->update_max_finished_tranc_id(tranc_id_);
+
+    spdlog::info(
+        "TranContext--commit(): Transaction ID={} committed successfully",
+        tranc_id_);
+
+    return true;
+  }
+
+  MemTable &memtable = engine_->memtable;
+  std::unique_lock<std::shared_mutex> wlock1(memtable.frozen_mtx);
+  std::unique_lock<std::shared_mutex> wlock2(memtable.cur_mtx);
+
+  if (isolation_level == IsolationLevel::REPEATABLE_READ ||
+      isolation_level == IsolationLevel::SERIALIZABLE) {
+
+    // 读不可重复读和串行的情况下，需要检测是否冲突
+
+    // SST的锁
+    std::shared_lock<std::shared_mutex> rlock3(engine_->ssts_mtx);
+
+    // 获取暂存的数据，查看数据是否在memtable和SST中已经存在
+    for (auto &[k, v] : temp_map_) {
+      auto res = memtable.get_(k, 0);
+
+      if (res.is_valid() && res.get_tranc_id() > tranc_id_) {
+        // memtable中存在这个键，且事务id大于暂存事务的id，表示写入内存表的事务更晚创建的，有更高的优先级，所以需要保留原本的，暂存的视为冲突
+        isAborted = true;
+        spdlog::warn("TranContext--commit(): Conflict detected on key={}, "
+                     "aborting transaction ID={}",
+                     k, tranc_id_);
+
+        return false;
+      } else {
+        // 需要判断事务id是否大于刷入的最大事务id，小于的话就不需要执行后续操作
+        if (tranManager_->get_max_flushed_tranc_id() <= tranc_id_) {
+          continue;
+        } else {
+          // 检查sst是否冲突
+          auto res = engine_->sst_get_(k, 0);
+          if (res.has_value()) {
+            auto [v, id] = res.value();
+            if (id > tranc_id_) {
+              // 存在相同的 key , 且其 tranc_id 大于暂存的
+              // tranc_id，表示发生了冲突
+              isAborted = true;
+              spdlog::warn("TranContext--commit(): SST conflict on key={}, "
+                           "aborting transaction ID={}",
+                           k, tranc_id_);
+              return false;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  // 没有冲突时，允许正常写入
+  operations.emplace_back(Record::commitRecord(this->tranc_id_));
+
+  auto wal_success = tranManager_->write_to_wal(operations);
+
+  if (!wal_success) {
+    spdlog::error(
+        "TranContext--commit(): Failed to write WAL for transaction ID={}",
+        tranc_id_);
+
+    throw std::runtime_error("write to wal failed");
+  }
+
+  // 将暂存数据应用到数据库
+  if (!test_fail) {
+    // 这里是手动调用 memtable 的无锁版本的 put_, 因为之前手动加了写锁
+    for (auto &[k, v] : temp_map_) {
+      memtable.put_(k, v, tranc_id_);
+    }
+  }
+
+  isCommited = true;
+  tranManager_->update_max_finished_tranc_id(tranc_id_);
+
+  spdlog::info(
+      "TranContext--commit(): Transaction ID={} committed successfully",
+      tranc_id_);
+
   return true;
 }
 
 bool TranContext::abort() {
   // TODO: Lab 5.2 abort 实现
+  auto isolation_level = get_isolation_level();
+  spdlog::info("TranContext--abort(): Aborting transaction ID={}", tranc_id_);
+
+  if (isolation_level == IsolationLevel::READ_UNCOMMITTED) {
+    // 读未提交时，手动回滚
+    for (auto &[k, res] : rollback_map_) {
+      if (res.has_value()) {
+        engine_->put(k, res.value().first, res.value().second);
+      } else {
+        // 之前本就不存在, 需要移除当前事务的新增操作
+        engine_->remove(k, tranc_id_);
+      }
+    }
+
+    isAborted = true;
+    spdlog::info("TranContext--abort(): Transaction ID={} aborted", tranc_id_);
+    return true;
+  }
+
+  isAborted = true;
   return true;
 }
 
@@ -161,6 +294,13 @@ TranManager::TranManager(std::string data_dir) : data_dir_(data_dir) {
   auto file_path = get_tranc_id_file_path();
 
   // TODO: Lab 5.2 初始化时读取持久化的事务状态信息
+
+  if (!std::filesystem::exists(file_path)) {
+    tranc_id_file_ = `FileObj::open(file_path, true);
+  } else {
+    tranc_id_file_ = FileObj::open(file_path, false);
+    read_tranc_id_file();
+  }
 }
 
 void TranManager::init_new_wal() {
@@ -175,18 +315,63 @@ TranManager::~TranManager() { write_tranc_id_file(); }
 
 void TranManager::write_tranc_id_file() {
   // TODO: Lab 5.2 持久化事务状态信息
+  // 共4个8字节的整型id记录需要持久化
+  // std::atomic<uint64_t> nextTransactionId_;
+  // std::atomic<uint64_t> max_flushed_tranc_id_;
+  // std::atomic<uint64_t> max_finished_tranc_id_;
+
+  std::vector<uint8_t> now_id(3 * sizeof(uint64_t), 0);
+  uint64_t nextTransactionId = nextTransactionId_.load();
+  uint64_t max_flushed_tranc_id = max_flushed_tranc_id_.load();
+  uint64_t max_finished_tranc_id = max_finished_tranc_id_.load();
+
+  auto ptr = now_id.data();
+  memcpy(ptr, &nextTransactionId, sizeof(uint64_t));
+  ptr += sizeof(uint64_t);
+
+  memcpy(ptr,&max_flushed_tranc_id, sizeof(uint64_t));
+  ptr += sizeof(uint64_t);
+
+  memcpy(ptr,&max_finished_tranc_id, sizeof(uint64_t));
+
+
+  // 写入磁盘，实现持久化
+  tranc_id_file_.write(0, now_id);
+  tranc_id_file_.sync();
+
+
 }
 
 void TranManager::read_tranc_id_file() {
   // TODO: Lab 5.2 读取持久化的事务状态信息
+  nextTransactionId_ = tranc_id_file_.read_uint64(0);
+  max_flushed_tranc_id_ = tranc_id_file_.read_uint64(sizeof(uint64_t));
+  max_finished_tranc_id_ = tranc_id_file_.read_uint64(sizeof(uint64_t) * 2);
 }
 
 void TranManager::update_max_finished_tranc_id(uint64_t tranc_id) {
   // TODO: Lab 5.2 更新持久化的事务状态信息
+  uint64_t expected = max_finished_tranc_id_.load(std::memory_order_relaxed);
+  while (tranc_id > expected) {
+    if (max_finished_tranc_id_.compare_exchange_weak(
+            expected, tranc_id, std::memory_order_acq_rel,
+            std::memory_order_relaxed)) {
+      break;
+    }
+  }
 }
 
 void TranManager::update_max_flushed_tranc_id(uint64_t tranc_id) {
   // TODO: Lab 5.2 更新持久化的事务状态信息
+  uint64_t expected = max_flushed_tranc_id_.load(std::memory_order_relaxed);
+  while (tranc_id > expected) {
+    if (max_flushed_tranc_id_.compare_exchange_weak(
+            expected, tranc_id, std::memory_order_acq_rel,
+            std::memory_order_relaxed)) {
+      break;
+    }
+  }
+  write_tranc_id_file();
 }
 
 uint64_t TranManager::getNextTransactionId() {
@@ -214,7 +399,7 @@ TranManager::new_tranc(const IsolationLevel &isolation_level) {
 
   auto tranc_id = getNextTransactionId();
 
-  //获取事务id对应的上下文
+  // 获取事务id对应的上下文
   activeTrans_[tranc_id] = std::make_shared<TranContext>(
       tranc_id, engine_, shared_from_this(), isolation_level);
 
